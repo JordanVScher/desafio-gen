cb7899c443cb62f49aa3c6cb9cb046736af80b0a
jordan <jordanvscher@hotmail.com>
2023-11-01 17:53:39 -0300

    feat: add env config to nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutoModule } from './produto/produto.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProdutoModule],
+  imports: [ProdutoModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
