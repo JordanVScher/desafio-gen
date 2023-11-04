feaf4812830fffbb0fca804b5c1e169d4fa0bfc8
jordan <jordanvscher@hotmail.com>
2023-11-04 14:36:16 -0300

    test: add config for mongo-memory-server; add test for post /categoria

--- src/categoria/categoria.controller.spec.ts
+++ src/categoria/categoria.controller.spec.ts
@@ -1,17 +1,32 @@
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { CategoriaModule } from './categoria.module';
+import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import {
+  closeMongodConnection,
+  rootMongooseTestModule,
+} from '../../test/test-utils/mongo/MongooseTestModule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Categoria, CategoriaSchema } from './categoria.schema';
+import { CategoriaController } from './categoria.controller';
+import { CategoriaService } from './categoria.service';
+import { CategoriaInformaticaStub } from '../../test/test-utils/stubs/categoriaStub';
 
 describe('Root', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [CategoriaModule],
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          { name: Categoria.name, schema: CategoriaSchema },
+        ]),
+      ],
+      controllers: [CategoriaController],
+      providers: [CategoriaService],
     }).compile();
 
-    app = moduleRef.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
@@ -19,14 +34,16 @@ describe('Root', () => {
     it(`create new categoria`, () => {
       return request(app.getHttpServer())
         .post('/categoria')
-        .send({ nome: 'Informatica' })
+        .send(CategoriaInformaticaStub)
         .expect(201)
         .then((res) => {
-          expect(res.text).toBe('This action adds a new categoria');
+          expect(res.body._id).toBeDefined();
+          expect(res.body.nome).toBe(CategoriaInformaticaStub.nome);
         });
     });
   });
   afterAll(async () => {
     await app.close();
+    await closeMongodConnection();
   });
 });
--- src/categoria/categoria.service.spec.ts
+++ src/categoria/categoria.service.spec.ts
@@ -1,11 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriaService } from './categoria.service';
+import { rootMongooseTestModule } from '../../test/test-utils/mongo/MongooseTestModule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Categoria, CategoriaSchema } from './categoria.schema';
 
 describe('CategoriaService', () => {
   let service: CategoriaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          { name: Categoria.name, schema: CategoriaSchema },
+        ]),
+      ],
       providers: [CategoriaService],
     }).compile();
 
